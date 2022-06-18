efb0719773b5ab3e328c9628f3f5ed8a0b6e237c
Antonyly <antonlyzun@gmail.com>
2022-06-18 20:34:47 +0300

    3-4 tasks

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,34 +3,34 @@ import PropTypes from 'prop-types';
 import s from './profile.module.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => (
- <div className={s.profile}>
-  <div className={s.description}>
-    <img
-      src={avatar}
-      alt={username}
-      className={s.avatar}
-    />
+  <div className={s.profile}>
+    <div className={s.description}>
+      <img
+        src={avatar}
+        alt={username}
+        className={s.avatar}
+      />
       <p className={s.name}>{username}</p>
-    <p className={s.tag}>@{tag}</p>
-      <p className={s.location}>{ location }</p>
-  </div>
+      <p className={s.tag}>@{tag}</p>
+      <p className={s.location}>{location}</p>
+    </div>
 
-  <ul className={s.stats}>
-    <li>
-      <span className={s.label}>Followers</span>
-        <span className={s.quantity}>{ stats.followers}</span>
-    </li>
-    <li>
-      <span className={s.label}>Views</span>
-      <span className={s.quantity}>{ stats.views}</span>
-    </li>
-    <li>
-      <span className={s.label}>Likes</span>
-      <span className={s.quantity}>{ stats.likes}</span>
-    </li>
-  </ul>
-</div>
-)
+    <ul className={s.stats}>
+      <li>
+        <span className={s.label}>Followers</span>
+        <span className={s.quantity}>{stats.followers}</span>
+      </li>
+      <li>
+        <span className={s.label}>Views</span>
+        <span className={s.quantity}>{stats.views}</span>
+      </li>
+      <li>
+        <span className={s.label}>Likes</span>
+        <span className={s.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
 
 
 Profile.propTypes = {
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import propTypes from 'prop-types';
 import s from './statistics.module.css';
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 const Statistics = ({title = 'Upload stats', stats }) => (
     <section className={s.statistics}>
         <h2 className={s.title}>{ title }</h2>
 
  <ul className={s.list}>
       {stats.map(el => (
-        <li className={s.item} key={el.id}>
+          <li className={s.item}
+              style={{
+            backgroundColor: getRandomHexColor(),
+              }}
+              key={el.id}>
           <span className={s.label}>{el.label}</span>
           <span className={s.percentage}>{el.percentage}%</span>
         </li>
